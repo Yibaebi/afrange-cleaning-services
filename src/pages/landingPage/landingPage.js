bc8e47643398d20443c00193b98eeae553a4b063
Yibaebi <elliot.yibaebi@yahoo.com>
2021-04-28 00:07:07 +0100

    Introduced hero and services section to landing page.

--- src/pages/landingPage/landingPage.js
+++ src/pages/landingPage/landingPage.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "./landingPage.css";
+import Hero from "./sections/hero";
+import Services from "./sections/services";
+import AboutUs from "./sections/aboutUs";
 
 const LandingPage = () => {
-  return <div>Home</div>;
+  return (
+    <main id="aae-landing-page" className="aae-landing-page--container">
+      <Hero />
+      <AboutUs />
+      <Services />
+    </main>
+  );
 };
 
 export { LandingPage };
