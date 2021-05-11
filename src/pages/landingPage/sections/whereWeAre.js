0c901454c0086c754c136eba0d6b518eab5534cf
Yibaebi <e.yibaebi@genesystechhub.com>
2021-05-11 09:40:49 +0100

    Created and styled Afrange's location in WhereWeAre component using google maps.

---
+++ src/pages/landingPage/sections/whereWeAre.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./whereWeAre.css";
+
+const WhereWeAre = () => {
+  return (
+    <div className="aae-where-we-are container-styles">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31715.859410723795!2d7.5000275!3d6.460398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1044a39738c9bf8d%3A0x169284b972b942e8!2sEkulu%20East%20Estate!5e0!3m2!1sen!2sng!4v1619970209493!5m2!1sen!2sng"
+        width="600"
+        height="450"
+        allowfullscreen=""
+        loading="lazy"
+        title="afrange locale"
+        className="mr-8"
+      ></iframe>
+      <aside className="aae-landing-hero--content ">
+        <div className="box">
+          <div className="circle">
+            <div className="inner"></div>
+            <div className="highlight"></div>
+          </div>
+          <div className="square"></div>
+        </div>
+        <div className="shadow"></div>
+        <button className="primary-button">
+          Get Directions <i className="fas fa-directions ml-2"></i>
+        </button>
+      </aside>
+    </div>
+  );
+};
+
+export default WhereWeAre;
