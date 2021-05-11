ffa88bf1839e00cca1390f1c94bbc8648ce10984
Yibaebi <e.yibaebi@genesystechhub.com>
2021-05-11 09:35:59 +0100

    Added extra-info bar to application. Styled to be fixed at the top of the application layout.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { primaryLogo } from "../../assets";
 import { NavBar } from "../navbar/navbar";
 import Router from "./../../router/index";
 
@@ -7,6 +8,28 @@ import "./app.css";
 const Afrange = () => {
   return (
     <div className="aae-main-app--container">
+      <aside className="aae-nav-bar--extra-info">
+        <div className="logo-container">{primaryLogo("white")}</div>
+        <div className="location-container">
+          <i className="fas fa-map-marker mr-4 fa-2x"></i>
+          <div className="aae-nav--contact-details">
+            <p>
+              5-7 Mandeville Pl, <br />
+              Marylebone, London W1U 3AY
+            </p>
+          </div>
+        </div>
+        <div className="location-container">
+          <i className="fas fa-clock mr-4 fa-2x"></i>
+          <div className="aae-nav--contact-details">
+            <p>
+              Mon - Fri, 08.00am - 06.00pm <br />
+              Saturday and Sunday closed.
+            </p>
+          </div>
+        </div>
+        <button className="secondary-button">Schedule Pickup</button>
+      </aside>
       <header>
         <NavBar />
       </header>
