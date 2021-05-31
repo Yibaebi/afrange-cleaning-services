073439aa1724586309408050bfb222b83379e910
Elliot Yibaebi <elliot.yibaebi@yahoo.com>
2021-05-31 18:21:22 +0100

    redesign about us section in landing page

--- src/pages/landingPage/sections/aboutUs.js
+++ src/pages/landingPage/sections/aboutUs.js
@@ -1,39 +1,153 @@
-import AboutUsImage from "../../../assets/landing-page/Ironing board.svg";
+import ClothFold from "../../../assets/landing-page/about-us-fold.jpg";
+import Cloth from "../../../assets/landing-page/about-us-image-1.jpg";
+import ClothIroning from "../../../assets/landing-page/about-us-image-ironing.jpg";
+import IndustryCleaning from "../../../assets/landing-page/about-us-industry-cleaning.jpg";
+import OfficeCleaning from "../../../assets/landing-page/about-us-office-cleaning.jpg";
+import ClothRepairs from "../../../assets/landing-page/about-us-image-cloth repairs.jpg";
+import ClothDelivery from "../../../assets/landing-page/about-us-image-delivery.jpg";
 
 import "./aboutUs.css";
+import { Link } from "react-router-dom";
 
 const AboutUs = () => {
   return (
     <section className="aae-landing-about-us--container container-styles">
       <section className="aae-landing-about-us--wrapper">
-        <aside className="aae-landing-about-us--image">
-          <img src={AboutUsImage} alt="" />
-        </aside>
         <aside className="aae-landing-about-us--content">
           <aside className="landing-services--content-header">
             <h1>
-              <span>We have a rich story of excellence... </span>
-              Our <em>History</em>
+              Defining the Future of <em>Cleaning Servies</em>
             </h1>
-          </aside>
-          <aside className="aae-landing-about-us--content-description">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-              corporis doloremque, tenetur reiciendis expedita similique totam
-              rerum, officia necessitatibus aut tempora ab esse id provident
-              nulla facilis enim dolorum aliquid!
+              BrainStation offers a full spectrum of cutting-edge digital
+              learning, giving professionals and organizations of all sizes the
+              skills they need to thrive in the digital economy.
             </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-              corporis doloremque, tenetur reiciendis expedita similique totam
-              rerum, officia necessitatibus aut tempora ab esse id provident
-              nulla facilis enim dolorum aliquid! Lorem ipsum dolor sit, amet
-              consectetur adipisicing elit. Iusto, ab iste explicabo, molestias
-              magnam, ad corporis sunt quasi cupiditate commodi debitis nulla
-              dolorem aperiam sint obcaecati nihil provident sequi tempore?
-            </p>
+            <div className="key-info__container">
+              <aside className="key-info__item">
+                <i className="fas fa-heart mr-5"></i>
+                <div className="key-info__item--stats">
+                  <h6>100%</h6>
+                  <p>Trusted</p>
+                </div>
+              </aside>
+              <aside className="key-info__item">
+                <i className="far fa-smile mr-5"></i>
+                <div className="key-info__item--stats">
+                  <h6>2,000+</h6>
+                  <p>Happy Clients</p>
+                </div>
+              </aside>
+              <aside className="key-info__item">
+                <i className="fas fa-chart-line mr-5"></i>
+                <div className="key-info__item--stats">
+                  <h6>99.9%</h6>
+                  <p>Customer Retention</p>
+                </div>
+              </aside>
+              <aside className="key-info__item">
+                <i className="fas fa-truck mr-5"></i>
+                <div className="key-info__item--stats">
+                  <h6>856+</h6>
+                  <p>Successful Deliveries</p>
+                </div>
+              </aside>
+            </div>
           </aside>
         </aside>
+        <div class="container">
+          <h3 className="gallery--header">
+            <i className="fa fa-globe mr-2"></i>Our Services
+          </h3>
+          <div class="gallery">
+            <figure class="gallery__item gallery__item--3">
+              <aside className="gallery__item--desc">
+                <Link to="/services">
+                  Dry Cleaning <i className="fas fa-caret-right ml-3"></i>
+                </Link>
+              </aside>
+              <img src={Cloth} alt="Gallery item 3" className="gallery__img" />
+            </figure>
+            <figure class="gallery__item gallery__item--4">
+              <aside className="gallery__item--desc">
+                <Link to="">
+                  Ironing <i className="fas fa-caret-right ml-3"></i>
+                </Link>
+              </aside>
+              <img
+                src={ClothIroning}
+                alt="Gallery item 4"
+                className="gallery__img"
+              />
+            </figure>
+
+            <figure class="gallery__item gallery__item--1">
+              <aside className="gallery__item--desc">
+                <Link to="">
+                  Industrial Cleaning
+                  <i className="fas fa-caret-right ml-3"></i>
+                </Link>
+              </aside>
+              <img
+                src={IndustryCleaning}
+                alt="Galler item 1"
+                className="gallery__img"
+              />
+            </figure>
+            <figure class="gallery__item gallery__item--2">
+              <aside className="gallery__item--desc">
+                <Link to="">
+                  Cloth Repairs <i className="fas fa-caret-right ml-3"></i>
+                </Link>
+              </aside>
+              <img
+                src={ClothRepairs}
+                alt="Gallery item 5"
+                className="gallery__img"
+              />
+            </figure>
+
+            <figure class="gallery__item gallery__item--7">
+              <aside className="gallery__item--desc">
+                <Link to="">
+                  Office Cleaning <i className="fas fa-caret-right ml-3"></i>
+                </Link>
+              </aside>
+              <img
+                src={OfficeCleaning}
+                alt="Gallery item 2"
+                className="gallery__img"
+              />
+            </figure>
+            <figure class="gallery__item gallery__item--5">
+              <aside className="gallery__item--desc">
+                <Link to="">
+                  Delivery <i className="fas fa-caret-right ml-3"></i>
+                </Link>
+              </aside>
+              <img
+                src={ClothDelivery}
+                alt="Gallery item 5"
+                className="gallery__img"
+              />
+            </figure>
+            <figure class="gallery__item gallery__item--6">
+              <aside className="gallery__item--desc">
+                <Link to="">
+                  Wash and Fold <i className="fas fa-caret-right ml-3"></i>
+                </Link>
+              </aside>
+              <img
+                src={ClothFold}
+                alt="Gallery item 6"
+                className="gallery__img"
+              />
+            </figure>
+          </div>
+        </div>
+        <button className="primary-button my-7">
+          See more information <i className="fas fa-caret-right ml-3"></i>
+        </button>
       </section>
     </section>
   );
