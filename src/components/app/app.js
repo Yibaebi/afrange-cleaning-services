4ac19bae4c6728f9953124d35b997239cb9690a1
Elliot Yibaebi <elliot.yibaebi@yahoo.com>
2021-05-31 20:52:17 +0100

    relocate and redesign whatsapp logo on landing page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,4 @@
+import { AnimatePresence } from "framer-motion";
 import React from "react";
 import { primaryLogo } from "../../assets";
 import { Footer } from "../footer/footer";
@@ -6,39 +7,51 @@ import Router from "./../../router/index";
 
 import "./app.css";
 
+/*
+ * FIXME:
+ */
+
 const Afrange = () => {
   return (
-    <div className="aae-main-app--container">
-      <aside className="aae-nav-bar--extra-info">
-        <div className="logo-container">{primaryLogo("white")}</div>
-        <div className="location-container">
-          <i className="fas fa-map-marker mr-4 fa-2x"></i>
-          <div className="aae-nav--contact-details">
-            <p>
-              5-7 Mandeville Pl, <br />
-              Marylebone, London W1U 3AY
-            </p>
+    <AnimatePresence>
+      <div className="aae-main-app--container">
+        <aside className="aae-nav-bar--extra-info">
+          <div className="logo-container">{primaryLogo("white")}</div>
+          <div className="location-container">
+            <i className="fas fa-map-marker mr-4 fa-2x"></i>
+            <div className="aae-nav--contact-details">
+              <p>
+                5-7 Mandeville Pl, <br />
+                Marylebone, London W1U 3AY
+              </p>
+            </div>
+          </div>
+          <div className="location-container">
+            <i className="fas fa-clock mr-4 fa-2x"></i>
+            <div className="aae-nav--contact-details">
+              <p>
+                Mon - Fri, 08.00am - 06.00pm <br />
+                Saturday and Sunday closed.
+              </p>
+            </div>
+          </div>
+          <button className="secondary-button">Schedule Pickup</button>
+        </aside>
+
+        <header>
+          <NavBar />
+        </header>
+        <main>
+          <Router />
+        </main>
+        <div type="button" class="email-bt">
+          <div class="text-call">
+            <i class="fab fa-whatsapp my-float"></i>
           </div>
         </div>
-        <div className="location-container">
-          <i className="fas fa-clock mr-4 fa-2x"></i>
-          <div className="aae-nav--contact-details">
-            <p>
-              Mon - Fri, 08.00am - 06.00pm <br />
-              Saturday and Sunday closed.
-            </p>
-          </div>
-        </div>
-        <button className="secondary-button">Schedule Pickup</button>
-      </aside>
-      <header>
-        <NavBar />
-      </header>
-      <main>
-        <Router />
-      </main>
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </AnimatePresence>
   );
 };
 
--- src/pages/landingPage/landingPage.js
+++ src/pages/landingPage/landingPage.js
@@ -15,15 +15,6 @@ const LandingPage = () => {
       <Services />
       <Pricing />
       <WhereWeAre />
-
-      <a
-        href="https://api.whatsapp.com/send?phone=08107837035&text=Hi%20There%21%20Welcome%20to%20Afrange!"
-        class="float"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <i class="fab fa-whatsapp my-float mr-3"></i> Contact Us
-      </a>
     </main>
   );
 };
