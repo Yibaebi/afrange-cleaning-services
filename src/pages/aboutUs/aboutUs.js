5c47f8afc2c6aeca1a448c96575acd7cec7c2d76
Elliot Yibaebi <elliot.yibaebi@yahoo.com>
2021-06-02 16:36:43 +0100

    complete about us page

--- src/pages/aboutUs/aboutUs.js
+++ src/pages/aboutUs/aboutUs.js
@@ -1,66 +1,115 @@
 import React from "react";
-import AboutUsImage from "../../assets/about-us/laundry-hero-image-about-us.jpg";
+import OurStory from "../../assets/about-us/our story.jpg";
+import AboutUsImg from "../../assets/about-us/laundry-hero-image-about-us.jpg";
 import "./aboutUs.css";
 
 const AboutUs = () => {
   return (
     <div className="aae-about-us--container">
       <header className="aae-about-us--header">
+        <div className="background-overlay"></div>
         <div className="mission-container">
-          <h1>Our Mission</h1>
+          <h1>Laundry, Dry Cleaning & Home Cleaning</h1>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque
-            dolorem saepe iusto? Quaerat nesciunt facere, corrupti adipisci
-            natus ea laboriosam beatae dolorum molestiae amet accusantium quos
-            possimus dicta animi. Quibusdam!
+            From home cleaning to dry cleaning and laundry we’re here to help,
+            every step of the way.
           </p>
-        </div>
-        <div className="about-us-image-container">
-          <img src={AboutUsImage} alt="" />
+          <div className="aae-about-us-hero--buttons">
+            <button className="primary-button mr-6">
+              Book a Service <i className="fas fa-arrow-right ml-2"></i>
+            </button>
+            <button className="secondary-button p-3">
+              <i className="fas fa-mail-bulk mr-2"></i> Send us feedback
+            </button>
+          </div>
         </div>
       </header>
 
       <main>
         <section className="aae-about-us--values">
-          <div className="landing-services--content-header">
+          <aside className="landing-services--content-header homes-buildings">
+            <i className="fas fa-braille fa-3x"></i>
+            <i className="fas fa-bolt fa-3x"></i>
             <h1>
-              Our <em>Values</em>
-              <span>We believe in the luxury that comes with variety... </span>
+              Powering <em>homes and buildings </em>
             </h1>
-          </div>
-          <aside className="aae-about-us--values-container">
-            <div>
-              <div className="about-us--values-container">
-                <h5 className="values-header">
-                  <i class="fas fa-soap fa-2x"></i>Clean Wash
-                </h5>
-                <p className="values-content">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Cupiditate harum doloribus quasi dignissimos et. Libero
-                  inventore nam non blanditiis cumque sunt. Sunt, dolorum
-                  voluptates! Dolorem eius minus praesentium porro
-                  incidunt.Sunt, dolorum voluptates! Dolorem eius minus
-                  praesentium porro incidunt.Sunt, dolorum voluptates! Dolorem
-                  eius minus praesentium porro incidunt.
+            <p>
+              We built our service to help both individuals and families as well
+              as real estate partners managing the buildings in our community..
+            </p>
+          </aside>
+          <aside className="aae-about-us--homes-buildings-container">
+            <div className="homes-buildings-container">
+              <div className="background-overlay homes-buildings"></div>
+              <div className="content-container">
+                <h5 className="homes-buildings-header">For Individuals</h5>
+                <p className="homes-buildings-content">
+                  Live in an area serviced Afrange? Receive concierge laundry,
+                  dry-cleaning, and home cleaning services at the tap of a
+                  button.
                 </p>
-              </div>
-              <div className="about-us--values-container">
-                <h5 className="values-header">
-                  <i class="fas fa-broom fa-2x"></i>
-                  Swift Delivery
-                </h5>
-                <p className="values-content">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Cupiditate harum doloribus quasi dignissimos et. Libero
-                  inventore nam non blanditiis cumque sunt. Sunt, dolorum
-                  voluptates! Dolorem eius minus praesentium porro incidunt.
-                </p>
+                <button className="secondary-button p-3">
+                  Find out more <i className="fas fa-arrow-right ml-2"></i>
+                </button>
               </div>
             </div>
+            <div className="homes-buildings-container">
+              <div className="background-overlay homes-buildings"></div>
+              <div className="content-container">
+                <h5 className="homes-buildings-header">For Buildings</h5>
+                <p className="homes-buildings-content">
+                  We work with some of the biggest names in real estate to help
+                  bring modern amenities their residents expect
+                </p>
+                <button className="primary-button">
+                  Find out more<i className="fas fa-arrow-right ml-2"></i>
+                </button>
+              </div>
+            </div>
+          </aside>
+        </section>
+
+        <section className="aae-about-us--our-story">
+          <aside className="our-story--image-container">
+            <div className="about-us-background-overlay"></div>
+            <img src={AboutUsImg} alt="our story" />
+          </aside>
+          <aside className="landing-services--content-header our-story">
+            <h1>
+              <em>Our Story</em>
+            </h1>
+            <p>
+              Since 2015, Afrange has been at the forefront of providing
+              accessible and convenient clothing care & home cleaning services.
+              We're bringing clean homes and clean clothes to these cities and
+              expanding fast.
+            </p>
+            <div className="aae-about-us-hero--buttons">
+              <button className="primary-button">
+                Find out more<i className="fas fa-arrow-right ml-2"></i>
+              </button>
+            </div>
+          </aside>
+        </section>
+        <section className="aae-about-us--values">
+          <aside className="landing-services--content-header known-for">
+            <i class="fas fa-broom fa-2x"></i>
+            <i class="fas fa-soap fa-2x"></i>
+            <i class="fas fa-laugh-beam fa-2x"></i>
+            <i class="fas fa-clock fa-2x"></i>
+            <h1>
+              What <em>We're Known</em> for
+            </h1>
+            <p>
+              We built our service to help both individuals and families as well
+              as real estate partners managing the buildings in our community..
+            </p>
+          </aside>
+          <aside className="aae-about-us--values-container">
             <div>
-              <div className="about-us--values-container">
+              <div className="about-us--values-container clean-wash">
                 <h5 className="values-header">
-                  <i class="fas fa-soap fa-2x"></i>Satisfaction
+                  <i class="fas fa-soap fa-2x"></i>Clean Wash
                 </h5>
                 <p className="values-content">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -68,22 +117,52 @@ const AboutUs = () => {
                   inventore nam non blanditiis cumque sunt.
                 </p>
               </div>
-              <div className="about-us--values-container">
+              <div className="about-us--values-container known-delivery">
                 <h5 className="values-header">
-                  <i class="fas fa-soap fa-2x"></i>
+                  <i class="fas fa-broom fa-2x"></i>
+                  Swift Delivery
+                </h5>
+                <p className="values-content">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Cupiditate harum doloribus quasi dignissimos et. Libero
+                  inventore nam non blanditiis cumque sunt.
+                </p>
+              </div>
+            </div>
+            <div>
+              <div className="about-us--values-container satisfaction">
+                <h5 className="values-header">
+                  <i class="fas fa-laugh-beam fa-2x"></i>Satisfaction
+                </h5>
+                <p className="values-content">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Cupiditate harum doloribus quasi dignissimos et. Libero
+                  inventore nam non blanditiis cumque sunt.
+                </p>
+              </div>
+              <div className="about-us--values-container service">
+                <h5 className="values-header">
+                  <i class="fas fa-clock fa-2x"></i>
                   24/7 Service
                 </h5>
                 <p className="values-content">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Cupiditate harum doloribus quasi dignissimos et. Libero
-                  inventore nam non blanditiis cumque sunt. Sunt, dolorum
-                  voluptates! Dolorem eius minus praesentium porro incidunt.
+                  inventore nam non blanditiis cumque sunt.
                 </p>
               </div>
             </div>
           </aside>
         </section>
       </main>
+      <section className="aae-services--schedule-pickup-section">
+        <aside className="schedule-pickup-section--content">
+          <h1>
+            Save 3+ hours a week when you leave your laundry to the experts
+          </h1>
+          <button className="secondary-button">Schedule Pickup</button>
+        </aside>
+      </section>
     </div>
   );
 };
