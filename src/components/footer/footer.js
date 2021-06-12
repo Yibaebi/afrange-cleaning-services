ddc4bd477cdc1d0c42286f1d015b2d1ac7e545a9
Elliot Yibaebi <elliot.yibaebi@yahoo.com>
2021-06-12 18:20:58 +0100

    complete footer responsiveness

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -16,69 +16,70 @@ const Footer = () => {
           </p>
         </div>
 
-        <div class="aae-footer--services">
-          <h6>Services</h6>
-          <ul class="footer-links">
-            <li>
-              <a href="http://scanfcode.com/category/c-language/">
-                Dry Clean Your Clothes
-              </a>
-            </li>
-            <li>
-              <a href="http://scanfcode.com/category/front-end-development/">
-                Winter Coat
-              </a>
-            </li>
-            <li>
-              <a href="http://scanfcode.com/category/back-end-development/">
-                Dry Clean
-              </a>
-            </li>
-            <li>
-              <a href="http://scanfcode.com/category/java-programming-language/">
-                Ironing Services
-              </a>
-            </li>
-            <li>
-              <a href="http://scanfcode.com/category/android/">Wash and Fold</a>
-            </li>
+        <div>
+          <div class="aae-footer--services">
+            <h6>Services</h6>
+            <ul class="footer-links">
+              <li>
+                <a href="http://scanfcode.com/category/front-end-development/">
+                  Winter Coat
+                </a>
+              </li>
+              <li>
+                <a href="http://scanfcode.com/category/back-end-development/">
+                  Dry Clean
+                </a>
+              </li>
+              <li>
+                <a href="http://scanfcode.com/category/java-programming-language/">
+                  Ironing Services
+                </a>
+              </li>
+              <li>
+                <a href="http://scanfcode.com/category/android/">
+                  Wash and Fold
+                </a>
+              </li>
 
-            <li>
-              <a href="http://scanfcode.com/contribute-at-scanfcode/">
-                Customer Care
-              </a>
-            </li>
-          </ul>
-        </div>
+              <li>
+                <a href="http://scanfcode.com/contribute-at-scanfcode/">
+                  Customer Care
+                </a>
+              </li>
+            </ul>
+          </div>
 
-        <div class="aae-footer--customer-care">
-          <h6>Customer Care</h6>
-          <ul class="footer-links">
-            <li>
-              <a href="http://scanfcode.com/about/">About Us</a>
-            </li>
-            <li>
-              <a href="http://scanfcode.com/contact/">Contact Us</a>
-            </li>
-            <li>
-              <a href="http://scanfcode.com/contribute-at-scanfcode/">
-                Pricing
-              </a>
-            </li>
+          <div class="aae-footer--customer-care">
+            <h6>Customer Care</h6>
+            <ul class="footer-links">
+              <li>
+                <a href="http://scanfcode.com/about/">About Us</a>
+              </li>
+              <li>
+                <a href="http://scanfcode.com/contact/">Contact Us</a>
+              </li>
+              <li>
+                <a href="http://scanfcode.com/contribute-at-scanfcode/">
+                  Pricing
+                </a>
+              </li>
 
-            <li>
-              <a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a>
-            </li>
-            <li>
-              <a href="http://scanfcode.com/sitemap/">Sitemap</a>
-            </li>
-          </ul>
-        </div>
-        <div className="aae-footer--contact-details">
-          <h6>Contact details</h6>
-          <p>5-7 Mandeville Pl, Marylebone, London W1U 3AY</p>
-          <p>info@laundrapp.com</p>
-          <p>Mon - Sun: 7:00 am - 11:00 pm</p>
+              <li>
+                <a href="http://scanfcode.com/privacy-policy/">
+                  Privacy Policy
+                </a>
+              </li>
+              <li>
+                <a href="http://scanfcode.com/sitemap/">Sitemap</a>
+              </li>
+            </ul>
+          </div>
+          <div className="aae-footer--contact-details">
+            <h6>Contact details</h6>
+            <p>5-7 Mandeville Pl, Marylebone, London W1U 3AY</p>
+            <p>info@laundrapp.com</p>
+            <p>Mon - Sun: 7:00 am - 11:00 pm</p>
+          </div>
         </div>
       </div>
       <div class="copy-social--container">
