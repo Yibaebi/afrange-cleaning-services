961a85cabc50a706b8c1f65db989e023d58bd21b
Elliot Yibaebi <elliot.yibaebi@yahoo.com>
2021-06-12 05:17:55 +0100

    update app complementary color, fixed hash routing (ignore previous commit which stated this) and made other random changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
 export * from "./landingPage/landingPage";
 export * from "./aboutUs/aboutUs";
 export * from "./services/services";
+export * from "./pricing/pricing";
+export * from "./contactUs/contactUs";
+export * from "./gallery/gallery";
--- src/pages/services/services.js
+++ src/pages/services/services.js
@@ -1,36 +1,70 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { DryCleaning } from "./serviceSections/dryCleaning";
+import { OtherServices } from "./serviceSections/otherServices";
+import { WashAndIron } from "./serviceSections/washAndIron";
 
 import "./services.css";
-import { DryCleaning } from "./serviceSections/dryCleaning";
-import { WashAndIron } from "./serviceSections/washAndIron";
 
 const Services = () => {
   return (
     <div className="aae-services container-styles">
       <nav className="aae-services--nav">
         <ul className={`aae-services-nav-bar--links-wrapper`}>
-          <NavLink className="aae-nav-link" to="/services/wash">
+          <NavLink
+            className="aae-nav-link"
+            to="#wash-and-iron"
+            isActive={() => {
+              if (
+                window.location.hash === "#wash-and-iron" ||
+                window.location.hash === ""
+              ) {
+                return true;
+              }
+            }}
+          >
             Wash and Fold
           </NavLink>
-          <NavLink className="aae-nav-link" to="/services/dry-clean">
+
+          <NavLink
+            className="aae-nav-link"
+            to="#dry-clean"
+            isActive={() => {
+              if (window.location.hash === "#dry-clean") {
+                return true;
+              }
+            }}
+          >
             Dry Cleaning
           </NavLink>
-          <NavLink className="aae-nav-link" to="/services/others">
+          <NavLink
+            className="aae-nav-link"
+            to="#other-services"
+            isActive={() => {
+              if (window.location.hash === "#other-services") {
+                return true;
+              }
+            }}
+          >
             Other Services
           </NavLink>
         </ul>
       </nav>
-
-      <DryCleaning />
-      <WashAndIron />
+      {window.location.hash === "#wash-and-iron" ||
+      window.location.hash === "" ? (
+        <WashAndIron />
+      ) : window.location.hash === "#dry-clean" ? (
+        <DryCleaning />
+      ) : (
+        <OtherServices />
+      )}
 
       <section className="aae-services--schedule-pickup-section">
         <aside className="schedule-pickup-section--content">
           <h1>
             Save 3+ hours a week when you leave your laundry to the experts
           </h1>
-          <button className="secondary-button">Schedule Pickup</button>
+          <button className="primary-button">Schedule Pickup</button>
         </aside>
         <aside className="schedule-pickup-section--overlay"></aside>
       </section>
